a3a67350ae1931e672202632fd0730a31c1bebe6
Michael Esch <eschmichael@yandex.com>
2019-09-27 19:50:37 -0400

    solved - pytest success

---
+++ 15/enumerate_data.py
@@ -0,0 +1,20 @@
+names = 'Julian Bob PyBites Dante Martin Rodolfo'.split()
+countries = 'Australia Spain Global Argentina USA Mexico'.split()
+
+
+def enumerate_names_countries():
+    """Outputs:
+       1. Julian     Australia
+       2. Bob        Spain
+       3. PyBites    Global
+       4. Dante      Argentina
+       5. Martin     USA
+       6. Rodolfo    Mexico"""
+    # breakpoint()
+    for enumerated_names in enumerate(names,1):
+        index = enumerated_names[0]
+        row = (f"{index}. "
+               f"{enumerated_names[1]:<11}"
+               f"{countries[index - 1]}")
+
+        print(row)
