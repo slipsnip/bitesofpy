1d7321c812c381092f67737efae46ce8d890e116
Michael Esch <eschmichael@yandex.com>
2019-09-29 18:20:39 -0400

    passed unit testing

--- 19/simple_property.py
+++ 19/simple_property.py
@@ -1,7 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 NOW = datetime.now()
 
 
 class Promo:
-    pass
+    def __init__(self, name_promotion, expires_datetime):
+        self.name_promotion = name_promotion
+        self.date_expires = expires_datetime
+
+    @property
+    def expired(self):
+        return (self.date_expires - NOW).days <= 0
