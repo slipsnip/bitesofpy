e0205dace30e020b51c44d46cb8e3fbcc1886acb
Michael Esch <eschmichael@yandex.com>
2019-09-29 20:30:11 -0400

    unit tests passed

--- 104/pipe.py
+++ 104/pipe.py
@@ -7,4 +7,4 @@ Become a PyBites ninja!"""
 def split_in_columns(message=message):
     """Split the message by newline (\n) and join it together on '|'
        (pipe), return the obtained output string"""
-    pass
+    return '|'.join(message.split('\n'))
