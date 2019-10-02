870afed5b86064524b7305405320f5cd4f5261ff
Michael Esch <eschmichael@yandex.com>
2019-10-02 19:07:28 -0400

    tests passed

--- 21/cars.py
+++ 21/cars.py
@@ -10,22 +10,28 @@ cars = {
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models
        (original order)"""
-    pass
+    return ', '.join(cars['Jeep'])
 
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    pass
+    return [cars[model][0] for model in cars]
 
 
 def get_all_matching_models(cars=cars, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+    all_models = [model for models in cars.values() for model in models]
+    greped_models = filter(lambda model: grep.lower() in model.lower(),
+                           all_models)
+    return sorted(greped_models)
 
 
 def sort_car_models(cars=cars):
     """return a copy of the cars dict with the car models (values)
        sorted alphabetically"""
-    pass
+    return {
+        manufacture: sorted(models)
+        for manufacture, models in cars.items()
+    }
