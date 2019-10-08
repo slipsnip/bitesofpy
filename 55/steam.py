9aea98c3e9817427bf468927024509f70850ea41
Michael Esch <eschmichael@yandex.com>
2019-10-08 17:04:07 -0400

    starting point

---
+++ 55/steam.py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+
+import feedparser
+
+# cached version to have predictable results for testing
+FEED_URL = "http://bit.ly/2IkFe9B"
+
+Game = namedtuple('Game', 'title link')
+
+
+def get_games():
+    """Parses Steam's RSS feed and returns a list of Game namedtuples"""
+    pass
