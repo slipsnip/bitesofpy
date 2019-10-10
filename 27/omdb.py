7538010244c270c46ac40e3e60bb1a6968cd3dbf
Michael Esch <eschmichael@yandex.com>
2019-10-10 18:59:35 -0400

    starting point

---
+++ 27/omdb.py
@@ -0,0 +1,34 @@
+import glob
+import json
+import os
+from urllib.request import urlretrieve
+
+BASE_URL = 'http://projects.bobbelderbos.com/pcc/omdb/'
+MOVIES = ('bladerunner2049 fightclub glengary '
+          'horrible-bosses terminator').split()
+TMP = '/tmp'
+
+# little bit of prework (yes working on pip installables ...)
+for movie in MOVIES:
+    fname = f'{movie}.json'
+    remote = os.path.join(BASE_URL, fname)
+    local = os.path.join(TMP, fname)
+    urlretrieve(remote, local)
+
+files = glob.glob(os.path.join(TMP, '*json'))
+
+
+def get_movie_data(files=files):
+    pass
+
+
+def get_single_comedy(movies):
+    pass
+
+
+def get_movie_most_nominations(movies):
+    pass
+
+
+def get_movie_longest_runtime(movies):
+    pass
