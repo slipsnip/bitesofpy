2f2095c5e1d1df2c88254c3238390d45fba3a5fc
Michael Esch <eschmichael@yandex.com>
2019-10-10 17:53:28 -0400

    passed unit tests

--- 44/license.py
+++ 44/license.py
@@ -1,1 +1,10 @@
-def gen_key(...
+from secrets import choice
+from string import ascii_uppercase
+from string import digits
+def gen_key(parts=4, chars_per_part=8):
+    valid_chars = ascii_uppercase + digits
+    key = ''
+    for index in range(1, parts + 1):
+        part = ''.join(choice(valid_chars) for count in range(chars_per_part))
+        key += (part, part + '-')[index < parts]
+    return key
