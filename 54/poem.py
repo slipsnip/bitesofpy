aef488143c1e8283bfc2f68c1f3214d001705d9e
Michael Esch <eschmichael@yandex.com>
2019-10-13 20:11:12 -0400

    passed unit tests

--- 54/poem.py
+++ 54/poem.py
@@ -1,5 +1,16 @@
+import re
+
+
 INDENTS = 4
 
 
 def print_hanging_indents(poem):
-    pass
+    poem_striped = poem.strip()
+    poem_cleaned = re.sub(r'[ ]{2,}', r'', poem_striped)
+
+    for paragraph in poem_cleaned.split('\n\n'):
+        for line in enumerate(paragraph.split('\n')):
+            if line[0] == 0:
+                print(line[1])
+            else:
+                print(' ' * INDENTS + line[1])
