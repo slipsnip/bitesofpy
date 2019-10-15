58e5ec90a7e8cf784345ba84654e7743595addff
Michael Esch <eschmichael@yandex.com>
2019-10-15 15:06:52 -0400

    passed unit tests

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -1,5 +1,9 @@
+from itertools import accumulate
+
+
 def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
-       returns a sequence of same length with the averages.
-       You can assume all items in sequence are numeric."""
-    pass
+    returns a sequence of same length with the averages.
+    You can assume all items in sequence are numeric."""
+    for count, added in enumerate(accumulate(sequence), 1):
+        yield round((added / count), 2)
