279b13549f3da535a91d93f86314ddd565404651
Michael Esch <eschmichael@yandex.com>
2019-10-16 11:46:06 -0400

    winner winner chicken dinner

--- 224/split.py
+++ 224/split.py
@@ -1,4 +1,12 @@
+import re
+
+
 def get_sentences(text):
     """Return a list of sentences as extracted from the text passed in.
        A sentence starts with [A-Z] and ends with [.?!]"""
-    pass
+    # get rid of pesky whitespace and blank lines
+    striped = re.sub(r'\n+', r' ', text.strip())
+    sentences = []
+    for sentence in re.findall(r'([A-Z].+?[\.\?\!])(?=(\s[A-Z])|$)', striped):
+        sentences.append(sentence[0])
+    return sentences
