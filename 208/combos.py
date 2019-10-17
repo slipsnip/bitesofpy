c3813eae31a9f97a7a7f2b64e719ee85b00a00e8
Michael Esch <eschmichael@yandex.com>
2019-10-17 18:34:25 -0400

    unit tests passed

--- 208/combos.py
+++ 208/combos.py
@@ -1,2 +1,9 @@
+from itertools import permutations
+
 def find_number_pairs(numbers, N=10):
-    pass
+    """ find pairs of numbers in numbers list that add up to N
+        for each pair in permutations of 2 numbers that add up to N sort then
+        retuple to correct for situations where two or more tuples are identicle
+        not by order but by value of the pair"""
+    pairs = {tuple(sorted(pair)) for pair in permutations(numbers, 2) if sum(pair) == N}
+    return list(pairs)
