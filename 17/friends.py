cfe61d4d5f84001f143d6a5c02fb8c85f121f573
Michael Esch <eschmichael@yandex.com>
2019-10-18 17:20:12 -0400

    fix inaccurate annotations of types

--- 17/friends.py
+++ 17/friends.py
@@ -1,8 +1,8 @@
 from typing import (List, Tuple)
 from itertools import (permutations, combinations)
 
-def friends_teams(friends: List[str],
-                  team_size: List[int] = 2,
+def friends_teams(friends: Tuple[str, ...],
+                  team_size: int = 2,
                   order_does_matter: bool = False) -> Tuple[str, ...]:
     if order_does_matter:
         return permutations(friends, team_size)
