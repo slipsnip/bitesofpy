e6aa8a5146ebbe95a1d7656e21f92d304f02b8ac
Michael Esch <eschmichael@yandex.com>
2019-10-18 17:13:30 -0400

    passed unit tests

--- 17/friends.py
+++ 17/friends.py
@@ -1,2 +1,10 @@
-def friends_teams():
-    pass
+from typing import (List, Tuple)
+from itertools import (permutations, combinations)
+
+def friends_teams(friends: List[str],
+                  team_size: List[int] = 2,
+                  order_does_matter: bool = False) -> Tuple[str, ...]:
+    if order_does_matter:
+        return permutations(friends, team_size)
+    else:
+        return combinations(friends, team_size)
