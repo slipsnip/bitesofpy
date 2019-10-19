7d52635def7ef02065cd36292920c1aa30bbc0f9
Michael Esch <eschmichael@yandex.com>
2019-10-19 18:32:36 -0400

    passed unit tests

--- 74/bday.py
+++ 74/bday.py
@@ -1,6 +1,22 @@
-import calendar
+from calendar import Calendar
 
 
 def weekday_of_birth_date(date):
     """Takes a date object and returns the corresponding weekday string"""
-    pass
+    cal = Calendar()
+
+    weekdays = "Monday \
+                Tuesday \
+                Wednesday \
+                Thursday \
+                Friday \
+                Saturday \
+                Sunday".split()
+    weekday_lookup = dict(
+        (day_number, day) for day_number, day in enumerate(weekdays))
+
+    return [
+        weekday_lookup[day_of_week]
+        for day_of_month, day_of_week in cal.itermonthdays2(date.year, date.month)
+        if day_of_month == date.day
+    ][0]
