06f752b8d3306093275f1c49c8fbcfdada14e1b1
Michael Esch <eschmichael@yandex.com>
2019-10-18 20:47:45 -0400

    unit tests passed

--- 67/calc_dts.py
+++ 67/calc_dts.py
@@ -7,9 +7,12 @@ pycon_date = date(2018, 5, 8)
 
 def get_hundred_days_end_date():
     """Return a string of yyyy-mm-dd"""
-    pass
+    days_100 = timedelta(days=100)
+    end_date = start_100days + days_100
+    return end_date.strftime('%Y-%m-%d')
 
 
 def get_days_between_pb_start_first_joint_pycon():
     """Return the int number of days"""
-    pass
+    days_between = pycon_date - pybites_founded
+    return days_between.days
