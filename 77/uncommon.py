365284657e28ee029caf77b93ef0e469534ed9bb
Michael Esch <eschmichael@yandex.com>
2019-10-20 11:41:44 -0400

    passed unit tests

--- 77/uncommon.py
+++ 77/uncommon.py
@@ -1,4 +1,8 @@
+from itertools import compress
+
+
 def uncommon_cities(my_cities, other_cities):
     """Compare my_cities and other_cities and return the number of different
        cities between the two"""
-    pass
+    uncommon = list(set(my_cities) ^ set(other_cities))
+    return len(uncommon)
