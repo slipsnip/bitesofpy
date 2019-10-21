e106057e363f4282a4ba01aa0d34b0316e9f4949
Michael Esch <eschmichael@yandex.com>
2019-10-21 19:41:28 -0400

    passed unit tests

--- 91/anyall.py
+++ 91/anyall.py
@@ -1,3 +1,6 @@
+import re
+
+
 VOWELS = 'aeiou'
 PYTHON = 'python'
 
@@ -5,16 +8,18 @@ PYTHON = 'python'
 def contains_only_vowels(input_str):
     """Receives input string and checks if all chars are
        VOWELS. Match is case insensitive."""
-    pass
+    # use regex for this one
+    vowels_found = re.findall(r'[aeiou]', input_str, flags=re.I)
+    return len(vowels_found) == len(input_str)
 
 
 def contains_any_py_chars(input_str):
     """Receives input string and checks if any of the PYTHON
        chars are in it. Match is case insensitive."""
-    pass
+    return any([True for char in input_str if char.upper() in PYTHON.upper()])
 
 
 def contains_digits(input_str):
     """Receives input string and checks if it contains
        one or more digits."""
-    pass
+    return re.search(r'\d+', input_str)
