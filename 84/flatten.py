e30152a6198c0883360c1cc471035db0ad70296b
Michael Esch <eschmichael@yandex.com>
2019-10-23 17:08:25 -0400

    passed unit tests

--- 84/flatten.py
+++ 84/flatten.py
@@ -1,2 +1,9 @@
 def flatten(list_of_lists):
-    pass
+    flat_list = []
+    for index, item in enumerate(list_of_lists):
+        if isinstance(item, tuple) or isinstance(item, list):
+            flat_list.extend(flatten(item))
+        else:
+            flat_list.append(item)
+
+    return flat_list
