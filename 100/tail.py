139176a6527f42a613f24e468bb8154f114d6c7a
Michael Esch <eschmichael@yandex.com>
2019-10-24 15:12:59 -0400

    passed unit tests

--- 100/tail.py
+++ 100/tail.py
@@ -1,4 +1,10 @@
+import re
+
+
 def tail(filepath, n):
     """Similate Unix' tail -n, read in filepath, parse it into a list,
        strip newlines and return a list of the last n lines"""
-    pass
+    with open(filepath) as file_fd:
+        lines = ''.join(file_fd.readlines())
+        lines = lines.splitlines()[-n:]
+        return lines
