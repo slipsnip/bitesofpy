0f4ac5ee1d49940b4283ed9b31c3c104c5a927fd
Michael Esch <eschmichael@yandex.com>
2019-10-26 16:19:12 -0400

    passed unit tests

--- 117/round_even.py
+++ 117/round_even.py
@@ -1,3 +1,11 @@
+from math import (floor, ceil)
+
+
 def round_even(number):
     """Takes a number and returns it rounded even"""
-    pass
+    if number % 0.5 != 0:
+        return round(number)
+    else:
+        return [value for value in
+                (floor(number), ceil(number))
+                if value % 2 == 0][0]
