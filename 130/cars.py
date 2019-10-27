a85ebdfcd792c5e2de0b4b0a32f03947c97819d5
Michael <eschmichael@yandex.com>
2019-10-27 17:17:10 -0400

    passed unit tests

--- 130/cars.py
+++ 130/cars.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from operator import itemgetter
 
 import requests
 
@@ -14,10 +15,15 @@ with requests.Session() as s:
 def most_prolific_automaker(year):
     """Given year 'year' return the automaker that released
        the highest number of new car models"""
-    pass
+    car_by_year = filter(lambda car: car.get('year') == year, data)
+    new_cars = set([(car.get('automaker'), car.get('model'))
+                    for car in car_by_year])
+    counted = Counter([car[0] for car in new_cars])
+    return max(counted.items(), key=itemgetter(1))[0]
 
 
 def get_models(automaker, year):
     """Filter cars 'data' by 'automaker' and 'year',
        return a set of models (a 'set' to avoid duplicate models)"""
-    pass
+    filtered = filter(lambda car: car.get('automaker') == automaker and car.get('year') == year, data)
+    return set([car.get('model') for car in filtered])
