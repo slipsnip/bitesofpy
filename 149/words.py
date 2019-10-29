87ee0fdc604ece7071aca11fc4ece4450b66fa43
Michael <eschmichael@yandex.com>
2019-10-29 17:57:15 -0400

    passed unit tests

--- 149/words.py
+++ 149/words.py
@@ -1,4 +1,5 @@
 def sort_words_case_insensitively(words):
     """Sort the provided word list ignoring case,
        one twist: numbers have to appear after letters!"""
-    pass
+    return sorted(words, key=lambda word: word.lower() if ord(word[0]) not in range(49,58) else chr(ord('z') + ord(word[0])))
+ 
