1d59acb015720431ee6e831e57e5449d9178ddd5
Michael <eschmichael@yandex.com>
2019-10-29 19:49:48 -0400

    passed unit tests

--- 153/rounding.py
+++ 153/rounding.py
@@ -1,6 +1,10 @@
+from decimal import (Decimal, Context, ROUND_UP, ROUND_DOWN, setcontext)
 def round_up_or_down(transactions, up=True):
     """Round the list of transactions passed in.
        If up=True (default) ronud up, else round down.
        Return a new list of rounded values
     """
-    pass
+    WHOLE_NUM = Decimal(1)
+    context = Context(rounding=ROUND_UP) if up else Context(rounding=ROUND_DOWN)
+    setcontext(context)
+    return [Decimal(num).quantize(WHOLE_NUM) for num in transactions]
