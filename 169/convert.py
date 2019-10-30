00db60eb64c9ff0128155e3f7adcffabab0066ac
Michael <eschmichael@yandex.com>
2019-10-30 19:58:55 -0400

    passed unit tests

--- 169/convert.py
+++ 169/convert.py
@@ -5,4 +5,9 @@ def convert(value: float, fmt: str) -> float:
     :param fmt: String indicating format to convert to
     :return: Float rounded to 4 decimal places after conversion
     """
-    pass
+    if not isinstance(value, (int, float)):
+        raise TypeError('must be of type int or float')
+    if fmt.lower() not in ['cm', 'in']:
+        raise ValueError(f"{fmt} must be one of 'cm', 'in'")
+    fmt = fmt.lower()
+    return round(value / 2.54, 4) if fmt == 'in' else round(value * 2.54, 4) 
