a876f3b02566a41fac3d122e44fa8df72cf9d5de
Michael <eschmichael@yandex.com>
2019-10-30 14:54:17 -0400

    using dirnames and zip

--- 161/tree.py
+++ 161/tree.py
@@ -6,5 +6,5 @@ def count_dirs_and_files(directory='.'):
     """Count the amount of of directories and files in passed in "directory" arg.
        Return a tuple of (number_of_directories, number_of_files)
     """
-    file_counts = [(1 if dir != directory else 0, len(files)) for dir, dirs, files in os.walk(directory)]
-    return reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), file_counts)
+    counts = [(len(dirs), len(files)) for dir, dirs, files in os.walk(directory)]
+    return tuple(sum(file_count) for file_count in zip(*counts))
