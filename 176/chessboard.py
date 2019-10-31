9af9feedca249816a35ae29f5067663c552c297d
Michael <eschmichael@yandex.com>
2019-10-31 17:15:22 -0400

    passed unit tests

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -4,4 +4,6 @@ WHITE, BLACK = ' ', '#'
 def create_chessboard(size=8):
     """Create a chessboard with of the size passed in.
        Don't return anything, print the output to stdout"""
-    pass
+    row = str(WHITE + BLACK) * int(size / 2)
+    board = [row if count % 2 == 0 else row[::-1] for count in range(size)]
+    print(*board, sep='\n')
