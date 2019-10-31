ff3908a39561277b7673741d51bc9925f19e63dc
Michael <eschmichael@yandex.com>
2019-10-31 19:19:22 -0400

    passed unit tests

--- 180/names.py
+++ 180/names.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from operator import itemgetter
+from itertools import groupby
 
 # fake data from https://www.mockaroo.com
 data = """last_name,first_name,country_code
@@ -15,5 +17,9 @@ Halbard,Davie,CN"""
 
 
 def group_names_by_country(data: str = data) -> defaultdict:
+    people = [tuple(record.split(',')[::-1]) for record in data.splitlines()[1:] if record.strip()]
     countries = defaultdict(list)
-    # you code
+    for country_code, first_name, last_name in people:
+        countries[country_code].append(f'{first_name} {last_name}')
+    return countries
+    
