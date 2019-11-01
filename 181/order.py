7516ebb2cac61ad957301c1da035f062e4f92170
Michael <eschmichael@yandex.com>
2019-10-31 20:00:13 -0400

    passed unit tests

--- 181/order.py
+++ 181/order.py
@@ -7,7 +7,7 @@ class OrderedList:
         self._numbers = []
 
     def add(self, num):
-        # you complete
+        bisect.insort(self._numbers, num)
 
     def __str__(self):
         return ', '.join(str(num) for num in self._numbers)
