0374e2c11de51da771e62d979cc76a9812f38aca
Michael <eschmichael@yandex.com>
2019-11-01 16:29:08 -0400

    adding Sphinx docstring and type annotations

--- 209/docstring.py
+++ 209/docstring.py
@@ -1,3 +1,13 @@
-def sum_numbers(numbers):
-    """Write your docstring here ..."""
+from typing import List
+
+
+def sum_numbers(numbers: List) -> int:
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int 
+    """
     pass
