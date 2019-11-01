0481695dbb5323bb884841b8f437d4d2bfeba8cc
Michael <eschmichael@yandex.com>
2019-11-01 15:19:56 -0400

    no need for raise StopIteration

--- 214/countdown.py
+++ 214/countdown.py
@@ -1,7 +1,4 @@
 def countdown():
     """Write a generator that counts from 100 to 1"""
     for count in range(100, 0, -1):
-        if count > 0:
-            yield count
-        else:
-            raise StopIteration
+        yield count
