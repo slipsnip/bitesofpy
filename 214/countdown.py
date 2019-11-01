6ea56c6e3dcaa50a583ac56d98b2bf4fedd77999
Michael <eschmichael@yandex.com>
2019-11-01 15:15:05 -0400

    passed unit test

--- 214/countdown.py
+++ 214/countdown.py
@@ -1,3 +1,7 @@
 def countdown():
     """Write a generator that counts from 100 to 1"""
-    pass
+    for count in range(100, 0, -1):
+        if count > 0:
+            yield count
+        else:
+            raise StopIteration
