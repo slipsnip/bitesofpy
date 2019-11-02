2daec6af267076f9a16a24c4e7559c91884e2c62
Michael <eschmichael@yandex.com>
2019-11-02 18:17:19 -0400

    passed unit tests

--- 189/control_flow.py
+++ 189/control_flow.py
@@ -1,7 +1,20 @@
+# names that start with IGNORE_CHAR are ignored,
+# names that have one or more digits are ignored,
+# if a name starts with QUIT_CHAR it inmediately exits the loop, so no more names are added/generated at this point (neither the QUIT_CHAR name),
+# return up till MAX_NAMES names max.
+
 IGNORE_CHAR = 'b'
 QUIT_CHAR = 'q'
 MAX_NAMES = 5
 
 
 def filter_names(names):
-    pass
+    count = 0
+    for name in names:
+        if name[0] == IGNORE_CHAR or not name.isalpha():
+            continue
+        if name[0] == QUIT_CHAR or count == MAX_NAMES:
+            break
+        count += 1
+        yield name
+
