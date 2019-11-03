140d6aba54dff6e4cd877e211f5aa69950ff8292
Michael <eschmichael@yandex.com>
2019-11-03 16:54:21 -0500

    passed unit tests

--- 185/spelling.py
+++ 185/spelling.py
@@ -1,6 +1,8 @@
-from difflib import SequenceMatcher
+from difflib import (SequenceMatcher, get_close_matches)
 from os import path
 from urllib.request import urlretrieve
+from collections import namedtuple
+from operator import attrgetter
 
 
 DICTIONARY = path.join('/tmp', 'dictionary.txt')
@@ -19,5 +21,15 @@ def suggest_word(misspelled_word: str, words: set = None) -> str:
        the entered misspelled word"""
     if words is None:
         words = load_words()
+    ScoredWord = namedtuple('ScoredWord', 'word score')
+    sm = SequenceMatcher()
+    sm.set_seq2(misspelled_word)
+
+    sugestions = get_close_matches(misspelled_word, words, n=5, cutoff=0.6)
+    # SequenceMatcher.set_seq1() always returns None thus always succeeds, gets executed
+    scored_sugestions = [ScoredWord(sugestion, sm.ratio()) for sugestion in sugestions if not sm.set_seq1(sugestion)]
+    return max(scored_sugestions, key=attrgetter('score')).word 
+
+
 
     # you code
