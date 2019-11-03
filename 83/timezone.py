6f506270a6e7207afaf4d114240f1966e5dfab1e
Michael <eschmichael@yandex.com>
2019-11-03 14:33:27 -0500

    passed unit tests

--- 83/timezone.py
+++ 83/timezone.py
@@ -7,4 +7,13 @@ SPAIN = timezone('Europe/Madrid')
 def what_time_lives_pybites(naive_utc_dt):
     """Receives a naive UTC datetime object and returns a two element
        tuple of Australian and Spanish (timezone aware) datetimes"""
-    pass
+    utc_dt = utc.localize(naive_utc_dt)
+    aus = utc_dt.astimezone(AUSTRALIA)
+    # aus = naive_utc_dt.astimezone(AUSTRALIA)
+    # sp = naive_utc_dt.astimezone(SPAIN)
+    # aus = AUSTRALIA.localize(utc_dt, is_dst=False)
+    # aus = AUSTRALIA.localize(naive_utc_dt.astimezone(AUSTRALIA))
+    # sp = SPAIN.localize(naive_utc_dt)
+    sp = aus.astimezone(SPAIN)
+
+    return (aus, sp)
