36611b08fbf109dc4b8d636d3adfdf04f2f78adb
Michael <eschmichael@yandex.com>
2019-11-04 15:19:00 -0500

    passed unit tests

--- 162/fill.py
+++ 162/fill.py
@@ -3,4 +3,4 @@ HTML_SPACE = '&nbsp;'
 
 def prefill_with_character(value, column_length=4, fill_char=HTML_SPACE):
     """Prepend value with fill_char for given column_length"""
-    pass
+    return f'{value:>{column_length}}'.replace(' ', fill_char)
