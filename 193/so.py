0a60eac34e4bff4efefd9ea59892366e6a531dff
Michael <eschmichael@yandex.com>
2019-11-04 18:18:10 -0500

    starting point

---
+++ 193/so.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+cached_so_url = 'https://bit.ly/2IMrXdp'
+
+
+def top_python_questions(url=cached_so_url):
+    """Use requests to retrieve the url / html,
+       parse the questions out of the html with BeautifulSoup,
+       filter them by >= 1m views ("..m views").
+       Return a list of (question, num_votes) tuples ordered
+       by num_votes descending (see tests for expected output).
+    """
+    pass
