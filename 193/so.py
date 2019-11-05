82fc8f131f67fa1eb7677b66cfbd110e7d41020f
Michael <eschmichael@yandex.com>
2019-11-05 15:08:47 -0500

    passed unit tests

--- 193/so.py
+++ 193/so.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from functools import partial
+from operator import itemgetter
+import re
 
 cached_so_url = 'https://bit.ly/2IMrXdp'
 
@@ -11,4 +14,15 @@ def top_python_questions(url=cached_so_url):
        Return a list of (question, num_votes) tuples ordered
        by num_votes descending (see tests for expected output).
     """
-    pass
+    response = requests.get(cached_so_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    question_summarys = soup.find_all(class_='question-summary')
+    questions = []
+    for summary in question_summarys:
+        vote_str = summary.find(class_='vote-count-post').string
+        question = summary.find(class_='question-hyperlink').string
+        views = summary.find(class_='views').string.strip()
+        questions.append((question, int(vote_str), views))
+    questions = [(question[0], question[1]) for question in questions if re.search(r'(\d)+(?=m)', question[-1])] 
+    return sorted(questions, key=itemgetter(1), reverse=True)
+
