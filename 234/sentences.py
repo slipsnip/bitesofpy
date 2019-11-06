eef16d7aa8e0cdb42c00cd90e3cef8a7032113eb
Michael <eschmichael@yandex.com>
2019-11-06 18:37:33 -0500

    passed unit tests

--- 234/sentences.py
+++ 234/sentences.py
@@ -1,4 +1,8 @@
+import re
+
+
 def capitalize_sentences(text: str) -> str:
     """Return text capitalizing the sentences. Note that sentences can end
        in dot (.), question mark (?) and exclamation mark (!)"""
-    pass
+    sentences = [sentence.strip() for sentence in re.split(r'(?<=[\.\!\?]) |$', text)]
+    return ' '.join(sentence[0].upper() + sentence[1:] for sentence in sentences if len(sentence) > 0)
