96bc4bd1068860759df18d5e4bafea0414d982c2
Michael <eschmichael@yandex.com>
2019-11-06 13:23:01 -0500

    passed unit tests

--- 132/vowels.py
+++ 132/vowels.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 VOWELS = list('aeiou')
 
 
@@ -5,4 +7,5 @@ def get_word_max_vowels(text):
     """Get the case insensitive word in text that has most vowels.
        Return a tuple of the matching word and the vowel count, e.g.
        ('object-oriented', 6)"""
-    pass
+    vowel_counts = [(word, sum(word.count(vowel) for vowel in VOWELS)) for word in text.split()]
+    return max(vowel_counts, key=itemgetter(1))
