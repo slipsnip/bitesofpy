86f720a674cf88e7e506d5a94d01b938bc7da3d5
Michael <eschmichael@yandex.com>
2019-11-10 18:39:59 -0500

    passed unit tests

--- 235/timings.py
+++ 235/timings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 from urllib.request import urlretrieve
+from operator import (itemgetter, attrgetter)
+from dataclasses import (dataclass, field)
+from typing import Tuple
 
 tmp = Path('/tmp')
 timings_log = tmp / 'pytest_timings.out'
@@ -10,6 +13,42 @@ if not timings_log.exists():
     )
 
 
+# PytestResult = namedtuple('PytestResult', 'byte num_passed total_seconds passed')
+
+@dataclass(order=True)
+class PytestResult(object):
+    byte: int
+    num_tests: int
+    total_seconds: float = field(metadata={'unit': 'seconds'})
+    passed: bool = field(default=False)
+    average: float = field(init=False)
+    sort_index: int = field(init=False, repr=False)
+
+
+    def __post_init__(self):
+        if self.passed:
+            self.average = self.total_seconds / self.num_tests
+            self.sort_index = self.average
+        else:
+            self.average = None
+            self.sort_index = 0
+
+
+def parse_pytest_string(record: str) -> Tuple[int]:
+    fields = record.split()
+    byte, num_tests, total_seconds = itemgetter(0, 2, -3)(fields)
+    byte = int(byte)
+    total_seconds = float(total_seconds)
+    if not num_tests.isnumeric():
+        return (byte, 0, 0.00) 
+    elif 'passed' not in fields:
+        return (byte, int(num_tests), total_seconds)
+    else:
+        return (byte, int(num_tests), total_seconds, True)
+
 def get_bite_with_fastest_avg_test(timings: list) -> str:
     """Return the bite which has the fastest average time per test"""
-    pass
+    # if index 2 of test result isnumeric passed is True, thus use as bool for passed
+    results = [PytestResult(*parse_pytest_string(record)) for record in timings]
+    fastest = min([result for result in results if result.passed], key=attrgetter('average'))
+    return fastest.byte
