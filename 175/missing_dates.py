f51db4ed32e1810da3ccd751110b5ceedca1fe63
Michael <eschmichael@yandex.com>
2019-11-09 19:46:31 -0500

    passed unit tests

--- 175/missing_dates.py
+++ 175/missing_dates.py
@@ -1,3 +1,7 @@
+import pandas
+from datetime import date
+
+
 def get_missing_dates(dates):
     """Receives a range of dates and returns a sequence
        of missing datetime.date objects (no worries about order).
@@ -7,4 +11,8 @@ def get_missing_dates(dates):
 
        See the Bite description and tests for example outputs.
     """
-    pass
+    # pandas series lets us sort by values and aparently suports dates
+    sorted_dates = pandas.Series(dates).sort_values()
+    # get first and last date from sorted to generate full range
+    full_range = pandas.date_range(sorted_dates.iloc[0], sorted_dates.iloc[-1])
+    return [date.date() for date in full_range if date.date() not in dates]
