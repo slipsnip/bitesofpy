86f0b34dde07d60c3cabd90f3cd48be8b87b63ed
Michael <eschmichael@yandex.com>
2019-11-10 20:01:11 -0500

    passed unit tests

--- 116/files.py
+++ 116/files.py
@@ -1,8 +1,10 @@
-import os
-
+from pathlib import Path
 ONE_KB = 1024
 
 
 def get_files(dirname, size_in_kb):
     """Return files in dirname that are >= size_in_kb"""
-    pass
+    working_dir = Path(dirname)
+    for file in working_dir.glob('*'):
+        if file.stat().st_size / ONE_KB >= size_in_kb:
+            yield file
