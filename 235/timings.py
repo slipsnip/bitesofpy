259723f3d29785c3c82bc3ddeaf3f5d020366833
Michael <eschmichael@yandex.com>
2019-11-10 19:07:22 -0500

    without sort_index and order=True Using overloaded __lt__ and float('inf')

--- 235/timings.py
+++ 235/timings.py
@@ -15,23 +15,26 @@ if not timings_log.exists():
 
 # PytestResult = namedtuple('PytestResult', 'byte num_passed total_seconds passed')
 
-@dataclass(order=True)
+@dataclass
 class PytestResult(object):
     byte: int
     num_tests: int
     total_seconds: float = field(metadata={'unit': 'seconds'})
     passed: bool = field(default=False)
     average: float = field(init=False)
-    sort_index: int = field(init=False, repr=False)
+    # sort_index: int = field(init=False, repr=False)
 
 
     def __post_init__(self):
         if self.passed:
             self.average = self.total_seconds / self.num_tests
-            self.sort_index = self.average
+            # self.sort_index = float(self.average)
         else:
-            self.average = None
-            self.sort_index = 0
+            self.average = float('inf')
+            # self.sort_index = float('inf')
+    
+    def __lt__(self, rhs):
+        return self.average < rhs.average
 
 
 def parse_pytest_string(record: str) -> Tuple[int]:
@@ -50,5 +53,5 @@ def get_bite_with_fastest_avg_test(timings: list) -> str:
     """Return the bite which has the fastest average time per test"""
     # if index 2 of test result isnumeric passed is True, thus use as bool for passed
     results = [PytestResult(*parse_pytest_string(record)) for record in timings]
-    fastest = min([result for result in results if result.passed], key=attrgetter('average'))
+    fastest = min(results)
     return fastest.byte
