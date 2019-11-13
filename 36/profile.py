18c86b11806b8188b5c476d7e3f73421c55daeb5
Michael <eschmichael@yandex.com>
2019-11-12 20:17:22 -0500

    passed unit tests

--- 36/profile.py
+++ 36/profile.py
@@ -1,2 +1,13 @@
-def get_profile():
-    pass
+def get_profile(name: str, age: int, *sports, **awards):
+    if not isinstance(age, int):
+        raise ValueError
+    if len(sports) > 5:
+        raise ValueError
+    stats = dict(name=name, age=age)
+    if sports:
+        stats['sports'] = sorted(list(sports))
+    if awards:
+        stats['awards'] = dict(awards)
+
+    return stats
+    
