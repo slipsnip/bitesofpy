e8c83320f04c2810582ed5982d82c7623aac9cd2
Michael <eschmichael@yandex.com>
2019-11-13 13:36:59 -0500

    passed unit tests

--- 159/calculator.py
+++ 159/calculator.py
@@ -1,3 +1,5 @@
+from operator import (add, sub, mul, truediv)
+
 def simple_calculator(calculation):
     """Receives 'calculation' and returns the calculated result,
 
@@ -11,4 +13,16 @@ def simple_calculator(calculation):
        Make sure you convert both numbers to ints.
        If bad data is passed in, raise a ValueError.
     """
-    pass
+    supported = '+ - * /'.split()
+    lhs, operator, rhs = calculation.split()
+    try:
+        if operator in supported:
+            # casting should raise ValueError if not int
+            operands = [int(operand) for operand in [lhs, rhs]]
+            calc = {'+': add, '-': sub, '*': mul, '/': truediv}
+            return calc.get(operator)(*operands)
+        else:
+            raise ValueError
+    except ZeroDivisionError:
+        raise ValueError
+    
