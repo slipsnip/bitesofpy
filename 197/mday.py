da537e68291bda2a5039c2adcb3339c2c7a307ca
Michael <eschmichael@yandex.com>
2019-11-15 15:03:09 -0500

    passed unit tests

--- 197/mday.py
+++ 197/mday.py
@@ -1,7 +1,12 @@
-from datetime import date
+from datetime import datetime
+from dateutil.rrule import (rrule, SU, YEARLY)
+from dateutil.parser import parse
 
 
 def get_mothers_day_date(year):
     """Given the passed in year int, return the date Mother's Day
        is celebrated assuming it's the 2nd Sunday of May."""
-    pass
+    start_date = parse(f"Jan {year}").date()
+    for date in rrule(YEARLY, dtstart=start_date, bymonth=5, byweekday=SU, bysetpos=2):
+        if date.year == year:
+            return date.date()
