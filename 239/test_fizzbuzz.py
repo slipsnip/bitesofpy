fec9ac72fb61ff6ec8f7f7eb98d9cfe6509f2c21
Michael <eschmichael@yandex.com>
2019-11-18 09:14:47 -0500

    unit tests written

--- 239/test_fizzbuzz.py
+++ 239/test_fizzbuzz.py
@@ -1,3 +1,19 @@
+import pytest
 from fizzbuzz import fizzbuzz
 
 # write one or more test functions below, they need to start with test_
+@pytest.mark.parametrize(
+    "number, ret",
+    [
+        (3, "Fizz"),
+        (5, "Buzz"),
+        (3, "Fizz"),
+        (15, "Fizz Buzz"),
+        (17, 17),
+        (5 * 8, "Buzz"),
+        (3 * 11, "Fizz"),
+    ],
+)
+def test_fizzbuzz_return(number, ret):
+    result = fizzbuzz(number)
+    assert result == ret
