791e1e2c341f74666c158f582ed8e8a9a64b0340
Michael <eschmichael@yandex.com>
2019-11-18 13:15:09 -0500

    passed unit tests

--- 155/split.py
+++ 155/split.py
@@ -10,4 +10,31 @@ def split_words_and_quoted_text(text):
        the resulting list would be:
        ['Should', 'give', '3 elements only']
     """
-    pass
+    # Without using regex split
+    # This method gives a pytest on my Ryzen 1700x CPU of
+    # ============================== 1 passed in 9.87s =============================== 
+    result = []
+    tmp = ''
+    inQuote = False
+
+    for i, char in enumerate(text):
+        if char == r'"':
+            if inQuote:
+                inQuote = False
+                result.append(tmp)
+                tmp = ''
+                continue
+            else:
+                inQuote = True
+                continue
+        if inQuote or char != ' ':
+            tmp += char
+            if i != len(text) - 1:
+                continue
+            else:
+                result.append(tmp)
+                break
+        if tmp != '':
+            result.append(tmp)
+            tmp = ''
+    return result
