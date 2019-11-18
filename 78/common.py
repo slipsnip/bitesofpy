7f00b5c7bdf898247609add33610f11b276c9d66
Michael <eschmichael@yandex.com>
2019-11-18 10:14:14 -0500

    passed unit tests

--- 78/common.py
+++ 78/common.py
@@ -1,4 +1,7 @@
+from functools import reduce
+
+
 def common_languages(programmers):
     """Receive a dict of keys -> names and values -> a sequence of
        of programming languages, return the common languages"""
-    pass
+    return reduce(lambda a, b: a & b, map(set, programmers.values()))
