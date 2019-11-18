4b1f07d4ac8ae100faede6f4d53249f16e290984
Michael <eschmichael@yandex.com>
2019-11-18 13:50:55 -0500

    using regex findall

--- 155/split.py
+++ 155/split.py
@@ -1,3 +1,6 @@
+import re
+
+
 def split_words_and_quoted_text(text):
     """Split string text by space unless it is
        wrapped inside double quotes, returning a list
@@ -10,31 +13,5 @@ def split_words_and_quoted_text(text):
        the resulting list would be:
        ['Should', 'give', '3 elements only']
     """
-    # Without using regex split
-    # This method gives a pytest on my Ryzen 1700x CPU of
-    # ============================== 1 passed in 9.87s =============================== 
-    result = []
-    tmp = ''
-    inQuote = False
-
-    for i, char in enumerate(text):
-        if char == r'"':
-            if inQuote:
-                inQuote = False
-                result.append(tmp)
-                tmp = ''
-                continue
-            else:
-                inQuote = True
-                continue
-        if inQuote or char != ' ':
-            tmp += char
-            if i != len(text) - 1:
-                continue
-            else:
-                result.append(tmp)
-                break
-        if tmp != '':
-            result.append(tmp)
-            tmp = ''
-    return result
+    # With regex split
+    return [''.join(group) for group in re.findall(r'(?<=\")(.+)(?=\")|(\b\w+\b)', text)]  
