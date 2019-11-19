9fd594e102374a54d444addda26e90847b08c50a
Michael <eschmichael@yandex.com>
2019-11-19 14:43:49 -0500

    passed unit tests

--- 70/eggs.py
+++ 70/eggs.py
@@ -1,7 +1,19 @@
 from random import choice
 
-COLORS = 'red blue green yellow brown purple'.split()
+COLORS = "red blue green yellow brown purple".split()
 
 
 class EggCreator:
-    pass
+    def __init__(self, limit):
+        self._limit = limit
+        self._count = 0  #  keep track of how many eggs
+
+    def __next__(self):
+        if self._count != self._limit:
+            self._count += 1
+            return f"{choice(COLORS)} egg"
+        else:
+            raise StopIteration
+
+    def __iter__(self):
+        return self
