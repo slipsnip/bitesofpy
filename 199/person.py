696bda2ac956d0d5149577693b12edab1f6fc928
Michael <eschmichael@yandex.com>
2019-11-20 15:00:28 -0500

    passed unit tests

--- 199/person.py
+++ 199/person.py
@@ -1,1 +1,16 @@
 # see __mro__ output in Bite description
+
+class Person(object):
+    def __str__(self):
+        return 'I am a person'
+    
+class Mother(Person):
+    def __str__(self):
+        return super().__str__() + ' and awesome mom'
+    
+class Father(Person):
+    def __str__(self):
+        return super().__str__ () + ' and cool daddy'
+class Child(Father, Mother):
+    def __str__(self):
+        return 'I am the coolest kid'
