5adeeb802c79424d188b0840c0d9222551e2b4bb
Michael <eschmichael@yandex.com>
2019-11-24 14:38:22 -0500

    passed unit tests

--- 82/score.py
+++ 82/score.py
@@ -3,7 +3,17 @@ from enum import Enum
 THUMBS_UP = '👍'  # in case you go f-string ...
 
 # move these into an Enum:
-BEGINNER = 2
-INTERMEDIATE = 3
-ADVANCED = 4
-CHEATED = 1
+
+class Score(Enum):
+    BEGINNER = 2
+    INTERMEDIATE = 3
+    ADVANCED = 4
+    CHEATED = 1
+
+    def __str__(self):
+        return f'{self.name} => {self.value * THUMBS_UP}'
+
+    @classmethod
+    def average(cls):
+        scores = [score.value for score in cls]
+        return sum(scores) / len(scores)
