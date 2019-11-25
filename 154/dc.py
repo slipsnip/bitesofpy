0fc440abc7d8d816dca74ead641176528348cacf
Michael <eschmichael@yandex.com>
2019-11-25 14:38:00 -0500

    passed unit tests

--- 154/dc.py
+++ 154/dc.py
@@ -1,1 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import (dataclass, field)
+
+@dataclass(order=True)
+class Bite(object):
+    number: int
+    title: str
+    level: str = field(default='Beginner')
+
+    def __post_init__(self):
+        self.title = self.title[0].upper() + self.title[1:]
