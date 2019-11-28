dfb99549ecd9270d8b79d3634749bc3a7cc1f7a3
Michael <eschmichael@yandex.com>
2019-11-28 14:24:14 -0500

    passed unit tests

--- 86/rgb2hex.py
+++ 86/rgb2hex.py
@@ -2,4 +2,8 @@ def rgb_to_hex(rgb):
     """Receives (r, g, b)  tuple, checks if each rgb int is within RGB
        boundaries (0, 255) and returns its converted hex, for example:
        Silver: input tuple = (192,192,192) -> output hex str = #C0C0C0"""
-    pass
+    
+    if any(color not in range(0, 256) for color in rgb):
+        raise ValueError
+    
+    return '#{:02X}{:02X}{:02X}'.format(*rgb)
