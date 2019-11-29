07881a326cc7b3a3ed01bf5de7bdc9d78cfa974a
Michael <eschmichael@yandex.com>
2019-11-29 15:17:01 -0500

    passed unit tests

--- 60/uno.py
+++ 60/uno.py
@@ -1,4 +1,10 @@
 from collections import namedtuple
+from itertools import chain, repeat, product
+
+"""There are 108 cards in a Uno deck. There are four suits, Red, Green, Yellow and Blue,
+each consisting of one 0 card, two 1 cards, two 2s, 3s, 4s, 5s, 6s, 7s, 8s and 9s; 
+    two Draw Two cards; two Skip cards; and two Reverse cards.
+In addition there are four Wild cards and four Wild Draw Four cards."""
 
 SUITS = 'Red Green Yellow Blue'.split()
 
@@ -9,4 +15,9 @@ def create_uno_deck():
     """Create a deck of 108 Uno cards.
        Return a list of UnoCard namedtuples
        (for cards w/o suit use None in the namedtuple)"""
-    pass
+    cards = [UnoCard(suit, face) for suit, face in product(SUITS, [0])]
+    two_of = chain(range(1, 10), 'Draw Two,Skip,Reverse'.split(','))
+    cards.extend(UnoCard(suit, face) for suit, face in product(SUITS, two_of) for _ in range(2))
+    four_of = 'Wild,Wild Draw Four'.split(',')
+    cards.extend(UnoCard(None, face) for _ in range(4) for face in four_of)
+    return cards
