3093ccff7dc620253e898a52eaf0520e60bd221b
Michael <eschmichael@yandex.com>
2019-11-30 18:58:59 -0500

    passed unit tests

--- 230/thumbs.py
+++ 230/thumbs.py
@@ -2,4 +2,13 @@ THUMBS_UP, THUMBS_DOWN = '👍', '👎'
 
 
 class Thumbs:
-    pass
+    def __mul__(self, rhs):
+        count = rhs
+        if count == 0:
+            raise ValueError('Specify a number')
+        thumb = THUMBS_UP if count > 0 else THUMBS_DOWN
+        count = abs(count)
+        return f'{thumb} ({count}x)' if count >=4 else thumb * count 
+    
+    def __rmul__(self, rhs):
+        return self.__mul__(rhs)
