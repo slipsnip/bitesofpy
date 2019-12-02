dc1549f13216c51a5fd06d6a176b871500acd991
Michael <eschmichael@yandex.com>
2019-12-02 13:14:54 -0500

    passed unit tests

--- 71/record.py
+++ 71/record.py
@@ -1,3 +1,10 @@
-class RecordScore():
+class RecordScore(object):
     """Class to track a game's maximum score"""
-    pass
+    def __call__(self, score):
+        try:
+            if score > self._top_score:
+                self._top_score = score
+        except AttributeError:
+            self._top_score = score
+        finally:
+            return self._top_score
