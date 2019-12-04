ae461840505b35c4c0e45aa48ef33c45c461017d
Michael <eschmichael@yandex.com>
2019-12-04 15:15:17 -0500

    passed unit tests

--- 72/belt.py
+++ 72/belt.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from itertools import takewhile
 
 scores = [10, 50, 100, 175, 250, 400, 600, 800, 1000]
 belts = 'white yellow orange green blue brown black paneled red'.split()
@@ -7,4 +8,7 @@ MIN_SCORE, MAX_SCORE = min(scores), max(scores)
 
 
 def get_belt(user_score):
-    pass
+    ranks = list(takewhile(lambda score: user_score >= score, HONORS))
+    if not ranks:
+        return None if len(ranks) == 0 else 'red'
+    return HONORS[ranks[-1]]
