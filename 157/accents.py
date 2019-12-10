767bd8aa74d78d5298f7e4440038bba64a2dccc6
Michael <eschmichael@yandex.com>
2019-12-10 12:23:58 -0500

    remove sort and test against lower case text

--- 157/accents.py
+++ 157/accents.py
@@ -5,4 +5,4 @@ def filter_accents(text):
     """Return a sequence of accented characters found in
        the passed in lowercased text string
     """
-    return  ''.join(sorted(letter.lower() for letter in text if 'WITH' in name(letter)))
+    return  ''.join(letter for letter in text.lower() if 'WITH' in name(letter))
