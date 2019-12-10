6f7051b52520df196875854beaf61079d2040d77
Michael <eschmichael@yandex.com>
2019-12-10 12:20:24 -0500

    passed unit tests

--- 157/accents.py
+++ 157/accents.py
@@ -1,5 +1,8 @@
+from unicodedata import name
+
+
 def filter_accents(text):
     """Return a sequence of accented characters found in
        the passed in lowercased text string
     """
-    pass
+    return  ''.join(sorted(letter.lower() for letter in text if 'WITH' in name(letter)))
