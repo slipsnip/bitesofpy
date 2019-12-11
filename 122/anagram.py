b9da0f3eb97adeacee84c36915525a5d955d8d8c
Michael <eschmichael@yandex.com>
2019-12-11 16:23:45 -0500

    passed unit tests

--- 122/anagram.py
+++ 122/anagram.py
@@ -1,5 +1,11 @@
-def is_anagram(word1, word2):
+from re import sub as substitute
+from functools import partial
+
+
+def is_anagram(*words):
     """Receives two words and returns True/False (boolean) if word2 is
        an anagram of word1, ignore case and spacing.
        About anagrams: https://en.wikipedia.org/wiki/Anagram"""
-    pass
+    word1, word2 = map(partial(substitute, r'\s', r''), [word.lower() for word in words])
+    return set(word1) ^ set(word2) == set() and \
+        len(word1) == len(word2)
