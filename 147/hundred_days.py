9bd94a193a39f94341bf421cbf46d0db17e607f0
Michael <eschmichael@yandex.com>
2019-12-12 13:21:28 -0500

    passed unit tests

--- 147/hundred_days.py
+++ 147/hundred_days.py
@@ -1,6 +1,7 @@
 from datetime import date
 
-import dateutil
+from dateutil import relativedelta
+from dateutil.rrule import *
 
 TODAY = date(year=2018, month=11, day=29)
 
@@ -9,4 +10,5 @@ def get_hundred_weekdays(start_date=TODAY):
     """Return a list of hundred date objects starting from
        start_date up till 100 weekdays later, so +100 days
        skipping Saturdays and Sundays"""
-    pass
+    weekdays = rrule(DAILY, dtstart=start_date, wkst=MO, byweekday=range(0,5), count=100)
+    return [day.date() for day in weekdays]
