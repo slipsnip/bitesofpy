d50e8fa3633398542fcad9b6e6945b1452ebe78e
Michael <eschmichael@yandex.com>
2019-12-13 18:27:01 -0500

    passed unit tests

--- 207/cached_property.py
+++ 207/cached_property.py
@@ -4,7 +4,14 @@ from time import sleep
 
 def cached_property(func):
     """decorator used to cache expensive object attribute lookup"""
-    ...
+    def wrapper(*args, **kwargs):
+        self = args[0] 
+        if self._mass:
+            return self._mass
+        else:
+            return func(*args, **kwargs)
+    return wrapper
+
 
 
 class Planet:
@@ -21,14 +28,11 @@ class Planet:
     def __repr__(self):
         return f'{self.__class__.__name__}({repr(self.color)})'
 
-    @property
+    @cached_property
     def mass(self):
         scale_factor = random()
         sleep(self.TEMPORAL_SHIFT)
         self._mass = (f'{round(scale_factor * self.GRAVITY_CONSTANT, 4)} '
                       f'{self.SOLAR_MASS_UNITS}')
         return self._mass
-
-    @mass.setter
-    def mass(self, value):
-        self._mass = value
+    mass = property(mass)
