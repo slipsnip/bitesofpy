4d61d3ac82880fd301571a1c276203b451f520fb
Michael <eschmichael@yandex.com>
2019-12-13 13:59:56 -0500

    passed unit tests

--- 99/sequence_generator.py
+++ 99/sequence_generator.py
@@ -1,2 +1,7 @@
+from string import ascii_uppercase, digits
+from itertools import cycle
+
+
 def sequence_generator():
-    pass
+    sequence = cycle(element for pair in zip(range(1, 27), ascii_uppercase) for element in pair)
+    yield from sequence
