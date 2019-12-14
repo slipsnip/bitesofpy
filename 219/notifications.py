22b148ec5760887420855794777813640bbb4d9c
Michael <eschmichael@yandex.com>
2019-12-14 14:38:31 -0500

    passed unit tests

--- 219/notifications.py
+++ 219/notifications.py
@@ -4,4 +4,10 @@ TODAY = date.today()
 
 
 def gen_bite_planning(num_bites=1, num_days=1, start_date=TODAY):
-    pass
+    num_days = timedelta(days=num_days)
+    current_date = start_date
+    while(True):
+        current_date += num_days
+        for _ in range(num_bites):
+            yield current_date
+
