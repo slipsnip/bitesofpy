3c0ba39430743e624d480bc2a52b517f73663561
Michael Esch <eschmichael@yandex.com>
2019-12-23 14:48:53 -0500

    passed unit tests

--- 41/login.py
+++ 41/login.py
@@ -1,12 +1,21 @@
+from functools import wraps
 known_users = ['bob', 'julian', 'mike', 'carmen', 'sue']
 loggedin_users = ['mike', 'sue']
 
 
 def login_required(func):
-    pass
+    @wraps(func)
+    def wrapper(*args, **argv):
+        user = args[0]
+        if user in known_users:
+            if user in loggedin_users:
+                return func(user, f'welcome back {user}')
+            return func(user, f'please login')
+        return func(user, 'please create an account')
+    return wrapper
 
 
 @login_required
-def welcome(user):
+def welcome(user, msg=''):
     '''Return a welcome message if logged in'''
-    pass
+    return msg
