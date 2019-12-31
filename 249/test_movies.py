63f1347ffbc5edea762d791c74b2d5df22c57248
Michael Esch <eschmichael@yandex.com>
2019-12-31 16:04:48 -0500

    starting point

---
+++ 249/test_movies.py
@@ -0,0 +1,31 @@
+import os
+
+import pytest
+
+from movies import MovieDb
+
+DB = os.path.join(os.getenv("TMP", "/tmp"), 'movies.db')
+# https://www.imdb.com/list/ls055592025/
+DATA = [
+    ("The Godfather", 1972, 9.2),
+    ("The Shawshank Redemption", 1994, 9.3),
+    ("Schindler's List", 1993, 8.9),
+    ("Raging Bull", 1980, 8.2),
+    ("Casablanca", 1942, 8.5),
+    ("Citizen Kane", 1941, 8.3),
+    ("Gone with the Wind", 1939, 8.1),
+    ("The Wizard of Oz", 1939, 8),
+    ("One Flew Over the Cuckoo's Nest", 1975, 8.7),
+    ("Lawrence of Arabia", 1962, 8.3),
+]
+TABLE = 'movies'
+
+
+@pytest.fixture
+def db():
+    # instantiate MovieDb class using above constants
+    # do proper setup / teardown using MovieDb methods
+    # https://docs.pytest.org/en/latest/fixture.html (hint: yield)
+
+
+# write tests for all MovieDb's query / add / delete
